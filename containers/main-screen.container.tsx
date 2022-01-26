7f68749294bbb02c954f512fa4cc3244029e807c
PlatonSterh <platon.s14@list.ru>
2022-01-26 18:34:57 +0500

    link in main screen

--- containers/main-screen.container.tsx
+++ containers/main-screen.container.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 import { ReactElement } from 'react';
 
 import Button from '../components/button.component';
@@ -15,28 +15,28 @@ interface MainScreenProps {
 }
 
 export const MainScreen = ({ title, description, image }: MainScreenProps) => {
-  const { push } = useRouter();
-
-  const redirectToCatalog = () => push('/catalog');
-
   return (
-    <div className='max-w-7xl mx-auto px-2 font-noto sm:px-6 lg:px-8 relative -top-0.5 mb-6 flex flex-col-reverse items-center sm:flex-row justify-between'>
-      <div className='flex flex-col justify-center max-w-md'>
-        <div className='text-primary text-2xl text-center sm:text-left'>
+    <div className="max-w-7xl mx-auto px-2 font-noto sm:px-6 lg:px-8 relative -top-0.5 mb-6 flex flex-col-reverse items-center sm:flex-row justify-between">
+      <div className="flex flex-col justify-center max-w-md">
+        <div className="text-primary text-2xl text-center sm:text-left">
           {title}
         </div>
-        <p className='text-primary my-4 text-center sm:text-justify'>
+        <p className="text-primary my-4 text-center sm:text-justify">
           {description}
         </p>
-        <Button className='sm:w-32 mt-6' onClick={redirectToCatalog}>В КАТАЛОГ</Button>
+        <Link href="/catalog">
+          <a>
+            <Button className="sm:w-32 mt-6">В КАТАЛОГ</Button>
+          </a>
+        </Link>
       </div>
       <div>
         <Image
           src={image.url}
-          alt='Beautiful bonsai'
+          alt="Beautiful bonsai"
           width={image.width}
           height={image.height}
-          className='relative -top-0.5'
+          className="relative -top-0.5"
         />
       </div>
     </div>
