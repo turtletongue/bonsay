afe506da0b27c82e2b64f1a9b69d2ed61ef06e08
PlatonSterh <platon.s14@list.ru>
2022-01-26 17:17:52 +0500

    og prefix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,10 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 
-const articlePrefix =
-  'og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#';
-
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
-        <Head prefix={articlePrefix} />
+        <Head prefix="og: http://ogp.me/ns#" />
         <body>
           <Main />
           <NextScript />
