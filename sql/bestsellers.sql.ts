3c8234529c928a28646469c6775568b2743cdddb
PlatonSterh <platon.s14@list.ru>
2022-01-26 20:58:09 +0500

    bestsellers sql fix

--- sql/bestsellers.sql.ts
+++ sql/bestsellers.sql.ts
@@ -3,16 +3,16 @@ export const bestsellers = {
   text: `
     SELECT product.id, product.name, product.price,
            product.height, (EXTRACT(year FROM AGE(product.birthdate))) AS age, upload.path,
-           (
+           COALESCE((
              SELECT SUM(qty)
              FROM purchases purchase
              WHERE purchase."productId" = product.id
-           ) AS sales
+           ), 0) AS sales
     FROM products product
     LEFT JOIN uploads upload
     ON product."uploadId" = upload.id
     GROUP BY product.id, upload.path
     ORDER BY sales DESC
     LIMIT 8
-  `
+  `,
 };
