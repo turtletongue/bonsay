8e5fbd37e9fa8c868acc30a985a273a4b9a69a53
PlatonSterh <platon.s14@list.ru>
2022-03-01 22:25:36 +0500

    fix phone input styles

--- components/phone-input.component.tsx
+++ components/phone-input.component.tsx
@@ -13,13 +13,13 @@ export const PhoneInput = ({ className, value, setPhone }: PhoneInputProps) => {
     setPhone(formatPhone(event.target.value));
 
   return (
-    <div className="flex w-100">
+    <div className={`flex ${className}`}>
       <span className="inline-flex items-center px-3 text-sm text-gray bg-white rounded-l-md border border-r-0 border-gray">
         +7
       </span>
       <input
         type="text"
-        className={`rounded-none rounded-r-lg form-control relative flex-auto min-w-0 block px-3 py-1.5 text-base font-normal text-gray bg-white bg-clip-padding border border-solid focus:bg-white focus:ring-green-700 focus:border-secondary border-gray ${className}`}
+        className="rounded-none rounded-r-lg form-control relative flex-auto min-w-0 block px-3 py-1.5 text-base font-normal text-gray bg-white bg-clip-padding border border-solid focus:bg-white focus:ring-green-700 focus:border-secondary border-gray"
         placeholder="(905) 033-5901"
         value={value}
         onChange={onPhoneChange}
