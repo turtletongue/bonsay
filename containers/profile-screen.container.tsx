7ce5c979bdf2a6ef11993e4069ea274421c82407
PlatonSterh <platon.s14@list.ru>
2022-03-01 23:25:54 +0500

    fix profile settings height

--- containers/profile-screen.container.tsx
+++ containers/profile-screen.container.tsx
@@ -8,7 +8,7 @@ interface ProfileScreenProps {
 
 export const ProfileScreen = ({ user }: ProfileScreenProps) => {
   return (
-    <div className='w-full p-4'>
+    <div className="w-full p-4 min-h-screen">
       <Settings user={user} />
     </div>
   );
