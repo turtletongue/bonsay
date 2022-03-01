2434a5185482620dd6273a654812b879fbe1943c
PlatonSterh <platon.s14@list.ru>
2022-03-01 23:42:14 +0500

    fix main screen

--- containers/main-screen.container.tsx
+++ containers/main-screen.container.tsx
@@ -16,28 +16,30 @@ interface MainScreenProps {
 
 export const MainScreen = ({ title, description, image }: MainScreenProps) => {
   return (
-    <div className="max-w-7xl mx-auto px-2 font-noto sm:px-6 lg:px-8 relative -top-0.5 mb-6 flex flex-col-reverse items-center sm:flex-row justify-between">
-      <div className="flex flex-col justify-center max-w-md">
-        <div className="text-primary text-2xl text-center sm:text-left">
-          {title}
+    <div className="w-full">
+      <div className="max-w-7xl mx-auto px-2 font-noto sm:px-6 lg:px-8 relative -top-0.5 mb-6 flex flex-col-reverse items-center sm:flex-row justify-between">
+        <div className="flex flex-col justify-center max-w-md">
+          <div className="text-primary text-2xl text-center sm:text-left">
+            {title}
+          </div>
+          <p className="text-primary my-4 text-center sm:text-justify">
+            {description}
+          </p>
+          <Link href="/catalog">
+            <a>
+              <Button className="sm:w-32 mt-6">В КАТАЛОГ</Button>
+            </a>
+          </Link>
         </div>
-        <p className="text-primary my-4 text-center sm:text-justify">
-          {description}
-        </p>
-        <Link href="/catalog">
-          <a>
-            <Button className="sm:w-32 mt-6">В КАТАЛОГ</Button>
-          </a>
-        </Link>
-      </div>
-      <div>
-        <Image
-          src={image.url}
-          alt="Beautiful bonsai"
-          width={image.width}
-          height={image.height}
-          className="relative -top-0.5"
-        />
+        <div>
+          <Image
+            src={image.url}
+            alt="Beautiful bonsai"
+            width={image.width}
+            height={image.height}
+            className="relative -top-0.5"
+          />
+        </div>
       </div>
     </div>
   );
