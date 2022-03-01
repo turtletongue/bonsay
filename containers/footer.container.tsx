53ddca8c5d5c78dcb3da50bf68b6207c36fd9ab1
PlatonSterh <platon.s14@list.ru>
2022-03-01 23:33:18 +0500

    fix footer

--- containers/footer.container.tsx
+++ containers/footer.container.tsx
@@ -18,37 +18,37 @@ const defaultUrls = {
   instagramUrl: 'https://instagram.com',
   facebookUrl: 'https://facebook.com',
   twitterUrl: 'https://twitter.com',
-  vkUrl: 'https://vk.com'
+  vkUrl: 'https://vk.com',
 };
 
 export const Footer = ({
   instagramUrl = defaultUrls.instagramUrl,
   facebookUrl = defaultUrls.facebookUrl,
   twitterUrl = defaultUrls.twitterUrl,
-  vkUrl = defaultUrls.vkUrl
+  vkUrl = defaultUrls.vkUrl,
 }: FooterProps) => {
   return (
-    <div className='w-full flex flex-col items-center bg-secondary p-8'>
-      <Logo color='text-white' />
-      <div className='flex justify-between w-44 m-4'>
+    <div className="w-full flex flex-col items-center bg-secondary p-8 mt-auto">
+      <Logo color="text-white" />
+      <div className="flex justify-between w-44 m-4">
         <Link href={instagramUrl}>
           <a>
-            <Image src={Instagram} alt='Instagram' width={30} height={29} />
+            <Image src={Instagram} alt="Instagram" width={30} height={29} />
           </a>
         </Link>
         <Link href={facebookUrl}>
           <a>
-            <Image src={Facebook} alt='Facebook' width={30} height={29} />
+            <Image src={Facebook} alt="Facebook" width={30} height={29} />
           </a>
         </Link>
         <Link href={twitterUrl}>
           <a>
-            <Image src={Twitter} alt='Twitter' width={30} height={29} />
+            <Image src={Twitter} alt="Twitter" width={30} height={29} />
           </a>
         </Link>
         <Link href={vkUrl}>
           <a>
-            <Image src={Vk} alt='Vk' width={30} height={29} />
+            <Image src={Vk} alt="Vk" width={30} height={29} />
           </a>
         </Link>
       </div>
